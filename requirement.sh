4f5430db125e3282f5825c2f465c4d677dd9bcaa
Sutariya Parixit <32614362+Bhai4You@users.noreply.github.com>
2018-03-10 02:57:43 +0000

    Add files via upload

---
+++ requirement.sh
@@ -0,0 +1,29 @@
+#!/bin/python
+# 
+#
+# Name	:	IP Attack
+# Code		:	python
+# Sec.Code	:	8h4i
+# Coder		: 	Sutariya Parixit
+# Site		: 	bhai4you.blogspot.com
+# github	:	github.com/Bhai4You
+#
+#
+#	<==(__WARNING__)==>
+#
+# DO NOT MODIFY OR COPY WITHOUT PERMISSION !!!
+#
+#	<==(__WARNING__)==>
+#
+#
+#
+apt update
+apt upgrade 
+apt install figlet -y
+apt install toilet -y
+apt install nano -y
+apt install ruby -y
+gem install lolcat
+clear
+toilet -f pagga "Done..!!" | lolcat
+
